f43fe0bc1ecf4d827e52869e431be6a8e46153a7
Nightmare23h <matthew.haldimann@besonet.ch>
2022-11-23 09:07:24 +0100

    Added new gamemode

--- app.py
+++ app.py
@@ -1,69 +1,40 @@
 from flask import Flask,render_template,request
-import sqlite3
-import random
-import sys
-
+from functions import yearfordriver, whodriver, yearforconstructor, whoconstructor
 
 app = Flask(__name__)
 
 
-def year():
-    con = sqlite3.connect("F1.db")  
-    cur = con.cursor()
-    randyear = random.randrange(72) + 1
-    cur.execute("SELECT year from years WHERE idYears = ?", (randyear,))
-    year = cur.fetchone()
-    yeartbd = str(year[0])
-    cur.close()
-    return yeartbd, randyear
-
-def whodriver(x):    
-    con = sqlite3.connect("F1.db")
-    cur = con.cursor()
-    cur.execute("SELECT driver_champion from years WHERE idYears = ?", (x,))
-    id = cur.fetchone()
-    cur.execute("SELECT name from drivers WHERE idDriver = ?", (id))
-    right_anwser = cur.fetchone()
-    driver = str(right_anwser[0])
-    cur.close()
-    return driver
-
-
-# This was my Way: 
-# @app.route('/form', methods = ['POST', 'GET'])
-# def data():
-#     if request.method == 'GET':
-#         func = year()
-#         ans = str(func[0])
-#         driver = whodriver(str(func[1]))
-#         return render_template("form.html", year=ans, test=driver )
-#     if request.method == 'POST':
-#         form_data = request.form
-#         driver2 = form_data["test"]
-#         if driver2 == form_data["answer"]:
-#             print("Gut")
-#         else: 
-#             print("Nix gut")
-#         func = year()
-#         ans = str(func[0])
-#         driver = whodriver(str(func[1]))
-#         return render_template("form.html", year=ans, test=driver )
-#  
 @app.route('/')
 def mainpage():
     return render_template("mainpage.html")
-    
+     
 
 # This is better (But not my way): 
+#Drivers Quiz Random
 @app.route('/rnddriver', methods = ['POST', 'GET'])
 def rnddriverquiz():
-    y, y_id = year() # for next game
+    y, y_id = yearfordriver() # for next game
     if request.method == 'POST':
         form_data = request.form
         winner = whodriver(form_data["year_id"])
         answer = form_data["answer"]
         if winner == answer:
-            return render_template("form.html", year=y, year_id=y_id, result="Correct!")
+            return render_template("rnddriver.html", year=y, year_id=y_id, result="Correct!")
         else: 
-            return render_template("form.html", year=y, year_id=y_id, result=f'Wrong! In the year {form_data["year"]} {winner} won!')
+            return render_template("rnddriver.html", year=y, year_id=y_id, result=f'Wrong! In the year {form_data["year"]} {winner} won!')
     return render_template("form.html", year=y, year_id=y_id)
+#Constructors Quiz Random
+@app.route('/rndcons', methods = ['POST', 'GET'])
+def rndconquiz():
+    y, y_id = yearforconstructor()
+    if request.method == 'POST':
+        form_data = request.form
+        winner = whoconstructor(form_data["year_id"])
+        answer = form_data["answer"]
+        if winner == answer:
+            return render_template("rndcon.html", year=y, year_id=y_id, result="Correct!")
+        else: 
+            return render_template("rndcon.html", year=y, year_id=y_id, result=f'Wrong! In the year {form_data["year"]} {winner} won!')
+    return render_template("rndcon.html", year=y, year_id=y_id)
+#Drivers Quiz IO
+
---
+++ functions.py
@@ -0,0 +1,49 @@
+import sqlite3
+import random 
+
+
+
+
+def yearfordriver():
+    con = sqlite3.connect("F1.db")  
+    cur = con.cursor()
+    randyeard = random.randrange(72) + 1
+    cur.execute("SELECT year from years WHERE idYears = ?", (randyeard,))
+    year = cur.fetchone()
+    yeartbddriver = str(year[0])
+    cur.close()
+    return yeartbddriver, randyeard
+
+def whodriver(x):    
+    con = sqlite3.connect("F1.db")
+    cur = con.cursor()
+    cur.execute("SELECT driver_champion from years WHERE idYears = ?", (x,))
+    id = cur.fetchone()
+    cur.execute("SELECT name from drivers WHERE idDriver = ?", (id))
+    right_anwser = cur.fetchone()
+    driver = str(right_anwser[0])
+    cur.close()
+    return driver
+
+
+def yearforconstructor():
+    con = sqlite3.connect("F1.db")
+    cur = con.cursor()
+    randyearc = random.randrange(64) + 1
+    cur.execute("SELECT year from years WHERE idYears = ?", (randyearc,))
+    year = cur.fetchone()
+    yeartbdcons = str(year[0])
+    cur.close()
+    return yeartbdcons, randyearc
+
+
+def whoconstructor(x):
+    con = sqlite3.connect("F1.db")
+    cur = con.cursor()
+    cur.execute("SELECT constructor_champion from years WHERE idYears = ?", (x,))
+    id = cur.fetchone()
+    cur.execute("SELECT name_cons FROM constructors WHERE idConstructors =?", (id))
+    right_anwser = cur.fetchone()
+    constructor = str(right_anwser[0])
+    cur.close()
+    return constructor
